5198653916af5db610daad79d185023cde9a8baa
Noah Clemmensen <noah0940@edu.sde.dk>
2026-05-18 12:25:37 +0200

    Add mobile topbar and bottom navigation; refactor modal and layout styles

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -13,6 +13,6 @@ class SettingsController extends AbstractController
     #[Route('/settings', name: 'app_settings')]
     public function index(): Response
     {
-        return $this->render('settings/index.html.twig');
+        return $this->render('discover/discover.html.twig');
     }
 }
