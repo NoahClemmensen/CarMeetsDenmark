ffe32d995db7d3d326c5d48abcd8db03c54a9261
Noah Clemmensen <noah0940@edu.sde.dk>
2026-05-29 09:14:13 +0200

    Better notfication position and fix margins on titles

--- assets/controllers/notification_bell_controller.js
+++ assets/controllers/notification_bell_controller.js
@@ -7,11 +7,18 @@ export default class extends Controller {
     connect() {
         this.outsideClickHandler = this.handleOutsideClick.bind(this);
         this.escapeHandler = this.handleEscape.bind(this);
+        this.resizeHandler = () => {
+            if (this.hasPanelTarget && this.panelTarget.dataset.open === 'true') {
+                this.closePanel();
+            }
+        };
+        window.addEventListener('resize', this.resizeHandler);
     }
 
     disconnect() {
         document.removeEventListener('click', this.outsideClickHandler);
         document.removeEventListener('keydown', this.escapeHandler);
+        window.removeEventListener('resize', this.resizeHandler);
     }
 
     async toggle(event) {
@@ -32,10 +39,8 @@ export default class extends Controller {
         panel.setAttribute('data-notification-bell-target', 'panel');
         panel.dataset.open = 'false';
         Object.assign(panel.style, {
-            position: 'absolute',
-            top: '100%',
-            right: '0',
-            marginTop: '0.5rem',
+            position: 'fixed',
+            right: '0.5rem',
             zIndex: '50',
             transition: 'opacity 180ms ease-out, transform 180ms ease-out',
             transformOrigin: 'top right',
@@ -44,6 +49,7 @@ export default class extends Controller {
             pointerEvents: 'none',
         });
         this.element.appendChild(panel);
+        this.positionPanel();
 
         try {
             const response = await fetch(this.urlValue, { headers: { Accept: 'text/html' } });
@@ -59,8 +65,14 @@ export default class extends Controller {
         }
     }
 
+    positionPanel() {
+        const rect = this.element.getBoundingClientRect();
+        this.panelTarget.style.top = `${rect.bottom + 8}px`;
+    }
+
     openPanel() {
         const p = this.panelTarget;
+        this.positionPanel();
         p.dataset.open = 'true';
         p.style.opacity = '1';
         p.style.transform = 'translateY(0) scale(1)';
